06e26506b7bcf5acf3be75c3f9c83660f9dea8b0
Szymon Lankiewicz <szymon.lankiewicz@gmail.com>
2018-12-20 07:44:41 +0100

    Added logs to producer code.

--- producer/src/main/java/com/devkwondo/benchmark/cache/producer/service/ProducerService.java
+++ producer/src/main/java/com/devkwondo/benchmark/cache/producer/service/ProducerService.java
@@ -3,6 +3,7 @@ package com.devkwondo.benchmark.cache.producer.service;
 import com.devkwondo.benchmark.cache.producer.configuration.properties.ProducerConfigurationProperties;
 import com.devkwondo.benchmark.cache.producer.model.Item;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Random;
@@ -10,6 +11,7 @@ import java.util.UUID;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
+@Slf4j
 @AllArgsConstructor
 @Service
 public class ProducerService {
@@ -19,15 +21,23 @@ public class ProducerService {
     private final Random random = new Random();
 
     public void populateCache() {
+        log.info("Populating cache using following settings: {}", producerConfigurationProperties);
         Executor executor = Executors.newFixedThreadPool(producerConfigurationProperties.getParallelism());
         for (int i = 0; i < producerConfigurationProperties.getItemsToProduce(); i++) {
             executor.execute(() -> {
-                Item item = createNewItem();
-                cachingService.addToCache(item.getId(), item);
-                cachingService.addToQueue(item.getId());
+                try {
+                    Item item = createNewItem();
+                    log.debug("Adding new item {}", item.getId());
+                    cachingService.addToCache(item.getId(), item);
+                    cachingService.addToQueue(item.getId());
+                    log.debug("New item was successfully added {}.", item.getId());
+                } catch (Exception e) {
+                    log.error(e.getMessage(), e);
+                }
+
             });
         }
-
+        log.info("Cache population finished.");
     }
 
     private Item createNewItem() {

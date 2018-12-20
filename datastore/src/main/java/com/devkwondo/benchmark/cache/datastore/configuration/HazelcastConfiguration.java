c7d29b1e1e7b24cf592f067a755207354a231365
Adrian Rogut <adrian.rogut@gmail.com>
2018-12-20 15:15:23 +0100

    Add queue config

--- datastore/src/main/java/com/devkwondo/benchmark/cache/datastore/configuration/HazelcastConfiguration.java
+++ datastore/src/main/java/com/devkwondo/benchmark/cache/datastore/configuration/HazelcastConfiguration.java
@@ -1,6 +1,5 @@
 package com.devkwondo.benchmark.cache.datastore.configuration;
 
-import com.hazelcast.config.CacheSimpleConfig;
 import com.hazelcast.config.Config;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
@@ -8,9 +7,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Profile("hazelcast")
 @Configuration
@@ -22,16 +18,8 @@ public class HazelcastConfiguration {
     }
 
     @Bean
-    Config config(List<CacheSimpleConfig> cacheConfigs) {
-        Config config = new Config();
-        cacheConfigs.forEach(config::addCacheConfig);
-        return config;
-    }
-
-    @Bean
     @ConfigurationProperties(prefix = "hazelcast.config")
-    List<CacheSimpleConfig> cacheConfigs() {
-        return new ArrayList<>();
+    Config config() {
+        return new Config();
     }
-
 }
